0d19cf3efc1c4587ac8bfbe8df3f69bad64ef750
shaun <shaunboo.22@ichat.sp.edu.sg>
2024-01-06 22:37:20 +0800

    SignUp1 layout done....

---
+++ app/src/main/java/com/sp/counterfit/SignUp1.java
@@ -0,0 +1,14 @@
+package com.sp.counterfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up1);
+    }
+}
