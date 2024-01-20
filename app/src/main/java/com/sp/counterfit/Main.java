f735254b50277169ba5a76dd8ef5f950c61030c0
shaun <shaunboo.22@ichat.sp.edu.sg>
2024-01-20 18:28:50 +0800

    Recommendation & steps ui done for main

--- app/src/main/java/com/sp/counterfit/Main.java
+++ app/src/main/java/com/sp/counterfit/Main.java
@@ -9,17 +9,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.Date;
+
+import java.util.Locale;
 
 public class Main extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -32,11 +38,14 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
     private ImageButton closeTipButton; // Button to close the tip
 
     private RelativeLayout tipContainer; // RelativeLayout for the tip of the day
+    private boolean isFirstLaunch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        isFirstLaunch = true;
 
         // Initialize UI components
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -48,6 +57,18 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
         textTipOfTheDay = findViewById(R.id.tipTextView);
         closeTipButton = findViewById(R.id.closeTipButton);
         tipContainer = findViewById(R.id.tipbg); // Assign the ID of the RelativeLayout
+        ImageView recoAddImageView = findViewById(R.id.reco_add);
+        setupBottomNavigationView();
+
+        recoAddImageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the RecommendActivity
+                Intent intent = new Intent(Main.this, Recommend.class);
+                startActivity(intent);
+            }
+        });
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -56,16 +77,29 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
         navigationView.setNavigationItemSelectedListener(this); // Set listener for top NavigationView
         botNavigationView.setNavigationItemSelectedListener(this);
 
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        SharedPreferences appPrefs = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        String lastShownDate = appPrefs.getString("LastShownDate", "");
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+
+        if (!lastShownDate.equals(currentDate)) {
+            displayRandomTip();
+            SharedPreferences.Editor editor = appPrefs.edit();
+            editor.putString("LastShownDate", currentDate);
+            editor.apply();
+        } else {
+            checkTipVisibility();
+        }
 
         closeTipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textTipOfTheDay.setVisibility(View.GONE); // Hide the tip TextView
-                closeTipButton.setVisibility(View.GONE); // Hide the close button
+                textTipOfTheDay.setVisibility(View.GONE);
+                closeTipButton.setVisibility(View.GONE);
                 tipContainer.setVisibility(View.GONE);
                 saveTipVisibilityState(false);
             }
@@ -148,6 +182,23 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+    private void setupBottomNavigationView() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id== R.id.bot_home){
+                    return true;
+                } else if (id==R.id.bot_food) {
+                    Intent intent = new Intent(Main.this, Home.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
+    }
 
     private void logoutUser() {
         // Clear the current session from the database
@@ -175,20 +226,25 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
         }
     }
     private void displayRandomTip() {
-        SharedPreferences sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
-        boolean isTipVisible = sharedPreferences.getBoolean("IsTipVisible", true);
+        SharedPreferences appPrefs = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        String lastTipDate = appPrefs.getString("LastTipDate", "");
 
-        if (!isTipVisible) {
-            tipContainer.setVisibility(View.GONE); // Hide the tip container if it was previously closed
-            return;
-        }
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String currentDate = dateFormat.format(new Date());
 
-        SignUpHelper dbHelper = new SignUpHelper(this);
-        String randomTip = dbHelper.getRandomTip(); // Method in SignUpHelper to get a random tip
-        if (randomTip != null) {
-            textTipOfTheDay.setText(randomTip);
-        } else {
-            textTipOfTheDay.setText("No tip for today. Stay tuned!"); // Default message
+        if (!currentDate.equals(lastTipDate)) {
+            // Display the tip and update the last shown date
+            SignUpHelper dbHelper = new SignUpHelper(this);
+            String randomTip = dbHelper.getRandomTip();
+            if (randomTip != null) {
+                textTipOfTheDay.setText(randomTip);
+            } else {
+                textTipOfTheDay.setText("No tip for today. Stay tuned!");
+            }
+
+            SharedPreferences.Editor editor = appPrefs.edit();
+            editor.putString("LastTipDate", currentDate);
+            editor.apply();
         }
     }
     private void saveTipVisibilityState(boolean isVisible) {
@@ -196,6 +252,16 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean("IsTipVisible", isVisible);
         editor.apply();
+    }
+    private void checkTipVisibility() {
+        SharedPreferences sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        boolean isTipVisible = sharedPreferences.getBoolean("IsTipVisible", true);
+
+        if (!isTipVisible) {
+            tipContainer.setVisibility(View.GONE); // Hide the tip container if it was previously closed
+        } else {
+            displayRandomTip(); // Display the tip if it is visible
+        }
     }
 
 
